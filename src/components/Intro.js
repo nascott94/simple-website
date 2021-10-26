f4ca1238dd6fc66834fef9035266d7374d9e75ef
nascott <nascott94@outlook.com>
2021-10-26 10:10:49 -0600

    added images to intro component

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -1,5 +1,6 @@
 import React from "react";
 import styled from "styled-components";
+import Woman from "../image/woman.jpg";
 
 const Container = styled.div`
   height: calc(100vh - 50px);
@@ -9,22 +10,86 @@ const Container = styled.div`
 
 const Left = styled.div`
   width: 60%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
 `;
 
 const Title = styled.h1`
+  width: 60%;
   font-size: 50px;
+`;
+
+const Desc = styled.p`
+  width: 60%;
+  font-size: 20px;
+  margin-top: 20px;
+`;
+
+const Info = styled.div`
+  width: 60%;
+  display: flex;
+  margin-top: 50px;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+const Button = styled.button`
+  padding: 15px;
+  background-color: darkblue;
+  color: white;
+  border-radius: 10px;
+  font-weight: bold;
+  border: none;
+  letter-spacing: 2px;
+  cursor: pointer;
+`;
+
+const Contact = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const Phone = styled.span`
+  color: pink;
+  font-weight: bold;
+`;
+
+const ContactText = styled.span`
+  color: grey;
+  margin-top: 5px;
 `;
 
 const Right = styled.div`
   width: 40%;
 `;
+
+const Image = styled.img`
+  width: 100%;
+`;
+
 export const Intro = () => {
   return (
     <Container>
       <Left>
         <Title>Adventures in creative age</Title>
+        <Desc>
+          We believe that designing products and services in close partnership
+          with our clients is the only way to have a real impact on their
+          business.
+        </Desc>
+        <Info>
+          <Button>START A PROJECT</Button>
+          <Contact>
+            <Phone>Call us (123)-456-7890</Phone>
+            <ContactText>For any questions or concerns</ContactText>
+          </Contact>
+        </Info>
       </Left>
-      <Right></Right>
+      <Right>
+        <Image src={Woman} />
+      </Right>
     </Container>
   );
 };
