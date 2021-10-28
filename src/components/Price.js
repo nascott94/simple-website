03328f3a1dba4d207731490f0c6456d6a198e032
nascott <nascott94@outlook.com>
2021-10-28 09:16:48 -0600

    added new components and shapes

---
+++ src/components/Price.js
@@ -0,0 +1,11 @@
+import React from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+  height: 100%;
+  display: flex;
+  justify-content: center;
+`;
+export const Price = () => {
+  return <Container></Container>;
+};
