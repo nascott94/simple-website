966939bbcbc61ef80f976ce8991f1a5d20e6b675
nascott <nascott94@outlook.com>
2021-10-28 11:21:32 -0600

    added contact and footer

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,9 @@ import { Navbar } from "./components/Navbar";
 import { Intro } from "./components/Intro";
 import Feature from "./components/Feature";
 import { Service } from "./components/Service";
+import { Price } from "./components/Price";
+import Contact from "./components/Contact";
+import { Footer } from "./components/Footer";
 
 const Container = styled.div`
   height: 100vh;
@@ -37,6 +40,12 @@ const ServiceShape = styled.div`
   background-color: #96a480;
 `;
 
+const PriceShape = styled.div`
+  ${Shape}
+  clip-path: polygon(33% 0%, 100% 0%, 100% 100%, 67% 100%);
+  background-color: #96a480;
+`;
+
 const App = () => {
   return (
     <>
@@ -52,6 +61,14 @@ const App = () => {
       <Container>
         <Service />
         <ServiceShape />
+      </Container>
+      <Container>
+        <Price />
+        <PriceShape />
+      </Container>
+      <Container>
+        <Contact />
+        <Footer />
       </Container>
     </>
   );
